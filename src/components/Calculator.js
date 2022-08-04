b85b6012a080737107b952a7af784df9ea1649c0
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-04 14:24:00 +0000

    Fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -26,22 +26,22 @@ class Calculator extends React.Component {
           <button type="button" id="AC" onClick={this.handleClick}>AC</button>
           <button type="button" id="+/-" onClick={this.handleClick}>+/-</button>
           <button type="button" id="%" onClick={this.handleClick}>%</button>
-          <button type="button" className="operator"  id="&divide;" onClick={this.handleClick}>&divide;</button>
+          <button type="button" className="operator" id="&divide;" onClick={this.handleClick}>&divide;</button>
           <button type="button" id="7" onClick={this.handleClick}>7</button>
           <button type="button" id="8" onClick={this.handleClick}>8</button>
           <button type="button" id="9" onClick={this.handleClick}>9</button>
-          <button type="button" className="operator"  id="x" onClick={this.handleClick}>x</button>
+          <button type="button" className="operator" id="x" onClick={this.handleClick}>x</button>
           <button type="button" id="4" onClick={this.handleClick}>4</button>
           <button type="button" id="5" onClick={this.handleClick}>5</button>
           <button type="button" id="6" onClick={this.handleClick}>6</button>
-          <button type="button" className="operator"  id="-" onClick={this.handleClick}>-</button>
+          <button type="button" className="operator" id="-" onClick={this.handleClick}>-</button>
           <button type="button" id="1" onClick={this.handleClick}>1</button>
           <button type="button" id="2" onClick={this.handleClick}> 2 </button>
           <button type="button" id="3" onClick={this.handleClick}> 3 </button>
-          <button type="button" className="operator"  id="+" onClick={this.handleClick}>+</button>
-          <button type="button" className="zero"  id="0" onClick={this.handleClick}>0</button>
+          <button type="button" className="operator" id="+" onClick={this.handleClick}>+</button>
+          <button type="button" className="zero" id="0" onClick={this.handleClick}>0</button>
           <button type="button" id="." onClick={this.handleClick}>.</button>
-          <button type="button" className="operator"  id="=" onClick={this.handleClick}>=</button>
+          <button type="button" className="operator" id="=" onClick={this.handleClick}>=</button>
         </section>
       </main>
     );
