cc8bd033db295f39cffdb49439cc9fc140668fb2
principlebrothers <principlesbrothers@yahoo.com>
2022-08-11 10:50:12 +0000

    Implement navbar router

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style/Nav.css'
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <div className="navContainer">
+      <div className="logoContainer">
+        <div className="logo"><span className='logoMath'>Math</span>magician</div>
+      </div>
+      <nav className="navLinks">
+        <Link to="/home">Home</Link>
+        <Link to="/calculator">Calculator</Link>
+        <Link to="/quotes">Quote</Link>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
